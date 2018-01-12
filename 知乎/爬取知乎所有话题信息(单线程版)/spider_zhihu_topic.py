a47575f5739bae91d1e2ca30ba0272e074f2e0be
tianhuahe <34700898+739561472@users.noreply.github.com>
2018-01-12 10:39:04 +0800

    update

--- 知乎/爬取知乎所有话题信息(单线程版)/spider_zhihu_topic.py
+++ 知乎/爬取知乎所有话题信息(单线程版)/spider_zhihu_topic.py
@@ -2,21 +2,20 @@
 
 from time import sleep
 
+import random
 import pymongo
 import requests
 import http.cookiejar
 import queue
-# from multiprocessing import Pool, Lock, Manager, Queue
+
+from multiprocessing import Pool, Lock, Manager, Queue
 from bs4 import BeautifulSoup
-from  collections import defaultdict
 from config import *
 
 client = pymongo.MongoClient()
 db = client[MONGO_DB]
 
-
 q = queue.Queue()
-# lock = Lock()
 
 session = requests.session()
 session.cookies = http.cookiejar.LWPCookieJar(filename='cookie')
@@ -30,14 +29,14 @@ headers = {
 }
 try:
     session.cookies.load(ignore_discard=True)
-except:
+except requests.HTTPError:
     print('cookie未加载')
 
 
 def get_html(url):
     try:
         res = session.get(url=url, headers=headers).text
-        sleep(0.5)
+        sleep(random.choice([0.5, 0.3, 0.7]))
         bs0bj = BeautifulSoup(res, 'lxml')
         return bs0bj
     except requests.HTTPError as e:
@@ -47,31 +46,29 @@ def get_html(url):
 def get_father_topic_data(bs0bj):
     try:
         father_topic_list = []
+        name = bs0bj.find('h1', {'class': 'zm-editable-content', 'data-disabled': '1'}).get_text()
         father_content = bs0bj.find('div', {'class': 'zm-side-section-inner parent-topic'}).findAll('a', {'class': 'zm-item-tag'})
         for s in father_content:
             father_topic_list.append(s.get_text())
-        return father_topic_list
-    except:
+        return father_topic_list, name
+    except AttributeError:
         print('该话题没有父话题！')
 
 
-def get_child_topic_data(bs0bj):
+def get_topic_token(bs0bj):
     try:
-        #child_topic_list = []
-        children_content = bs0bj.find('div', {'class': 'zm-side-section-inner child-topic'}).findAll('a', {'class': 'zm-item-tag'})
-        for child_topic in children_content:
-            if 'data-token' in child_topic.attrs:
-                data_token = child_topic.attrs['data-token']
-                get_ajax_data(data_token)
-                url = 'https://www.zhihu.com/topic/{data_token}/hot'.format(data_token=data_token)
-                q.put(url)
-                # child_topic_list.append({
-                #     'data_token': data_token,
-                #     'child_topic_name': child_topic.get_text()
-                # })
-        # return child_topic_list
-    except:
-        print('该话题没有子话题！')
+        children_content = bs0bj.find('div', {'class': 'zm-side-section-inner child-topic'}).findAll('a', {
+            'class': 'zm-item-tag'
+        })
+        if children_content:
+            for child_topic in children_content:
+                if 'data-token' in child_topic.attrs:
+                    data_token = child_topic.attrs['data-token']
+                    get_ajax_data(data_token)
+                    url = 'https://www.zhihu.com/topic/{data_token}/hot'.format(data_token=data_token)
+                    q.put(url)
+    except AttributeError:
+        print('该话题没有子话题')
 
 
 def get_ajax_data(data_token):
@@ -79,54 +76,42 @@ def get_ajax_data(data_token):
     bs0bj = get_html(ajax_url)
     name = bs0bj.find('span', {'class', 'name'}).get_text()
     content_value = bs0bj.findAll('span', {'class', 'value'})
-    # 获取相应问题，关注的url
-    # content_url = bs0bj.findAll('a', {'class', 'item'})
     question_num = content_value[0].get_text()
     followers = content_value[2].get_text()
     topic = {
         'name': name,
         '问题数': question_num,
-        '关注数': followers
+        '关注数': followers,
     }
     save_to_mongo(topic)
 
 
-def tree():
-    return defaultdict(tree)
-
-
-# def data_handler(bs0bj):
-#     father_topic_list = get_father_topic_data(bs0bj)
-#     child_topic_list = get_child_topic_data(bs0bj)
-#     for child_topic in child_topic_list:
-#         if 'data_token' in child_topic.keys:
-#             get_ajax_data(child_topic['data_token'])
-#             child_topic_name = child_topic['child_topic_name']
-
-
-def save_to_mongo(topic):
-    if db[MONGO_TOPIC_TABLE].insert(topic):
-        print('插入成功！')
-        return
+def save_to_mongo(topic_dict=None, father_topic=None, name=None):
+    if topic_dict:
+        if db[MONGO_TOPIC_TABLE].insert(topic_dict):
+            print('话题插入成功！')
+            return True
+    else:
+        if db[MONGO_TOPIC_TABLE].update({'name': name}, {'$set': {'父话题': father_topic}}):
+            print('父话题插入成功！')
+            return True
 
 
 def controller(url):
     bs0bj = get_html(url)
-    # father_topic = get_father_topic_data(bs0bj)
-    get_child_topic_data(bs0bj)
+    get_topic_token(bs0bj)
+    if get_father_topic_data(bs0bj):
+        father_topic_list, name = get_father_topic_data(bs0bj)
+        save_to_mongo(father_topic=father_topic_list, name=name)
 
 
 def main():
     start_url = 'https://www.zhihu.com/topic/19776749/hot'
     controller(start_url)
-    # pool = Pool(processes=3)
     while not q.empty():
         next_url = q.get()
         controller(next_url)
-        # pool.map(controller, next_url)
 
 
 if __name__ == '__main__':
-    # manager = Manager()
-    # q = manager.Queue()
     main()
